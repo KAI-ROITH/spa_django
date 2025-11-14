730880aa701fa191c17622b636188aca4b16f1f2
KAI-ROITH <harithikmal818@gmail.com>
2025-11-14 12:08:59 +0800

    Add root redirect to /accounts/

--- spa_django/urls.py
+++ spa_django/urls.py
@@ -4,10 +4,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
+    path('', RedirectView.as_view(url='/accounts/', permanent=True)),
 ]
 
 # Serve media files in development
